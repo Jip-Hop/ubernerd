69e7f1074cb248af31e8282d95a415e2511a7cc5
Jip-Hop <2871973+Jip-Hop@users.noreply.github.com>
2023-08-28 21:02:41 +0200

    Update ubernerd.sh

--- ubernerd.sh
+++ ubernerd.sh
@@ -2,15 +2,19 @@
 
 set -euo pipefail
 
-ABSOLUTE_SCRIPT_PATH="$(realpath "${BASH_SOURCE[0]}")"
-SCRIPT_NAME=$(basename "${ABSOLUTE_SCRIPT_PATH}")
-SCRIPT_PARENT_DIR="$(dirname "$ABSOLUTE_SCRIPT_PATH")"
+# Set the CREATE_SYMLINK environment variable to 1 to make the nerdctl command available globally
+# This will modify the host rootfs
+CREATE_SYMLINK="${CREATE_SYMLINK:-0}"
+
+absolute_script_path="$(realpath "${BASH_SOURCE[0]}")"
+script_name=$(basename "${absolute_script_path}")
+script_parent_dir="$(dirname "$absolute_script_path")"
 
 # Function to show error message and line number
 show_error() {
 	local line=$1
 	local line_content
-	mapfile -s $((line - 1)) -n 1 -t line_content <"$ABSOLUTE_SCRIPT_PATH"
+	mapfile -s $((line - 1)) -n 1 -t line_content <"$absolute_script_path"
 	echo "An error occurred in the script at line $line:"
 	echo "${line_content[0]}"
 }
@@ -24,7 +28,17 @@ fail() {
 
 [[ -z "${BASH_VERSINFO+x}" ]] && fail 'This script must run in bash...'
 [[ $UID -ne 0 ]] && fail "Run this script as root..."
-cd "${SCRIPT_PARENT_DIR}" || fail "Could not change working directory to ${SCRIPT_PARENT_DIR}..."
+
+# Check if ubernerd-containerd is already running, else don't continue and tell user to manually stop it first
+if systemctl is-active --quiet "ubernerd-containerd"; then
+	echo 'Abort: ubernerd-containerd is already running!'
+	echo 'You may stop it with: systemctl stop ubernerd-containerd.'
+	fail 'Be aware that this will stop all running containers!'
+else
+	echo 'Welcome ubernerd!'
+fi
+
+cd "${script_parent_dir}" || fail "Could not change working directory to ${script_parent_dir}..."
 
 stat_chmod() {
 	# Only run chmod if mode is different from current mode
@@ -69,17 +83,16 @@ mygrep() {
 }
 
 # Set appropriate permissions (if not already set) for this file, since it's executed as root
-stat_chmod 700 "${SCRIPT_NAME}"
+stat_chmod 700 "${script_name}"
 
 # Prepend nerdctl binaries to path
-export PATH="${SCRIPT_PARENT_DIR}/nerdctl_full/bin:${PATH}"
+export PATH="${script_parent_dir}/nerdctl_full/bin:${PATH}"
 
-# TODO: check if ubernerd-containerd is already running, else don't continue and tell user to manually stop it first
-
+echo ''
 echo "Creating directories (if necessary)..."
 
 # List of directories to create
-dir_list=("config/cni/net.d" "config/containerd" "config/nerdctl" "nerdctl_full" "plugins/containerd" "state/containerd/root" "state/containerd/state" "state/nerdctl/data_root")
+dir_list=("config/cni/net.d" "config/containerd" "config/nerdctl" "nerdctl_full" "state/containerd/root" "state/containerd/state" "state/nerdctl/data_root")
 
 # Loop through the list and create directories
 for dir_name in "${dir_list[@]}"; do
@@ -89,25 +102,29 @@ done
 # Check if containerd and nerdctl inside nerdctl_full, else download and extract latest release
 
 if [[ -f 'nerdctl_full/bin/containerd' && -f 'nerdctl_full/bin/nerdctl' ]]; then
-	echo 'Found containerd and nerdctl inside nerdctl_full directory.'
-	echo 'No need to download...'
+	echo ''
+	echo 'Found containerd and nerdctl inside nerdctl_full directory (no need to download).'
 else
-	echo 'Need to download latest release of nerdctl.'
+	echo ''
+	echo 'Need to download latest release of nerdctl...'
 
 	manualy_download_extract_message='Please manually download the latest nerdctl-full from https://github.com/containerd/nerdctl/releases, extract it and place contents in the nerdctl_full directory.'
 
 	if ! is_executable_available "wget" && ! is_executable_available "curl"; then
-		echo "Neither wget or curl are avaialble..."
+		echo ''
+		echo "Neither wget or curl are avaialble."
 		fail "$manualy_download_extract_message"
 	fi
 
 	if ! is_executable_available 'tar'; then
-		echo 'The tar command is not available...'
+		echo ''
+		echo 'The tar command is not available.'
 		fail "$manualy_download_extract_message"
 	fi
 
 	download_url="$(download 'https://api.github.com/repos/containerd/nerdctl/releases/latest' | mygrep 'browser_download_url' | mygrep 'nerdctl-full-' | mygrep '-linux-amd64.tar.gz' | cut -d '"' -f 4)"
 
+	echo ''
 	echo "Downloading $download_url..."
 	download "$download_url" | tar -xz -C 'nerdctl_full'
 fi
@@ -157,19 +174,20 @@ nerdctl_script_contents=$(
 export PATH="$PATH"
 
 # Make nerdctl use our config file
-export NERDCTL_TOML="${SCRIPT_PARENT_DIR}/config/nerdctl/nerdctl.toml"
+export NERDCTL_TOML="${script_parent_dir}/config/nerdctl/nerdctl.toml"
 
 # Make nerdctl use custom directories and pass through all command line arguments
 nerdctl \\
-	--data-root "${SCRIPT_PARENT_DIR}/state/nerdctl/data_root" \\
-	--cni-path "${SCRIPT_PARENT_DIR}/nerdctl_full/libexec/cni" \\
-	--cni-netconfpath "${SCRIPT_PARENT_DIR}/config/cni/net.d" \\
+	--data-root "${script_parent_dir}/state/nerdctl/data_root" \\
+	--cni-path "${script_parent_dir}/nerdctl_full/libexec/cni" \\
+	--cni-netconfpath "${script_parent_dir}/config/cni/net.d" \\
 	"\$@"
 EOF
 )
 
 # Create the nerdctl bash script (if not exists or contents are different)
 if [[ ! -f nerdctl || ! "$(cat nerdctl)" = "$nerdctl_script_contents" ]]; then
+	echo ''
 	echo 'Creating nerdctl script'
 	printf "%s" "$nerdctl_script_contents" >nerdctl
 fi
@@ -177,13 +195,33 @@ fi
 # Make nerdctl executable if not already
 stat_chmod 755 nerdctl
 
-# TODO: create symlink to nerdctl ONLY if enabled via env variable (don't create by default as technically it modifies host rootfs and it's not portable)
-# TODO: update symlink if it's pointing at wrong path
-if [[ ! -f /usr/local/sbin/nerdctl ]]; then
-	echo "Creating symlink for nerdctl..."
-	ln -s "${SCRIPT_PARENT_DIR}/nerdctl" /usr/local/sbin/nerdctl
+if [[ "${CREATE_SYMLINK}" -eq 1 ]]; then
+	symlink_path=/usr/local/sbin/nerdctl
+	correct_destination="${script_parent_dir}/nerdctl"
+
+	if [[ ! -f /usr/local/sbin/nerdctl ]]; then
+		echo ''
+		echo 'Creating symlink for nerdctl...'
+		ln -s "$correct_destination" "$symlink_path"
+	elif [[ -L "$symlink_path" ]]; then
+		if [[ "$(readlink "$symlink_path")" != "$correct_destination" ]]; then
+			echo ''
+			echo 'Updating symlink for nerdctl...'
+			ln -sf "$correct_destination" "$symlink_path"
+		fi
+	else
+		echo ''
+		echo "File exists at $symlink_path but it is not a symlink..."
+		echo 'Skipped creating symlink.'
+	fi
+else
+	echo ''
+	echo 'The CREATE_SYMLINK environment variable is not set to 1.'
+	echo 'Skipped creating/updating nerdctl symlink.'
+	echo 'By default ubernerd will not create a symlink on the rootfs of the host.'
 fi
 
+echo ''
 echo "Enabling IP forwarding..."
 echo 1 >/proc/sys/net/ipv4/ip_forward
 
@@ -193,18 +231,21 @@ if [[ 'failed' == $(systemctl show -p ActiveState --value 'ubernerd-containerd')
 	systemctl reset-failed ubernerd-containerd
 fi
 
+echo ''
 echo "Starting containerd..."
 
 # Unset trap, no need to know line numbers from here on
 trap - ERR
 
 # Make containerd use custom directories and pass through all command line arguments
+# Use KillMode=mixed instead of KillMode=process to also kill subprocesses (including all running containers),
+# otherwise there's no way to shutdown ubernerd and run this script again
 systemd-run \
 	--unit='ubernerd-containerd' \
 	--description='containerd container runtime started by ubernerd' \
 	--collect \
 	-p Delegate=yes \
-	-p KillMode=process \
+	-p KillMode=mixed \
 	-p LimitNPROC=infinity \
 	-p LimitCORE=infinity \
 	-p LimitNOFILE=infinity \
@@ -213,11 +254,13 @@ systemd-run \
 	--setenv=PATH \
 	-- \
 	containerd \
-	--config "${SCRIPT_PARENT_DIR}/config/containerd/config.toml" \
-	--root "${SCRIPT_PARENT_DIR}/state/containerd/root" \
-	--state "${SCRIPT_PARENT_DIR}/state/containerd/state"
+	--config "${script_parent_dir}/config/containerd/config.toml" \
+	--root "${script_parent_dir}/state/containerd/root" \
+	--state "${script_parent_dir}/state/containerd/state"
 
-echo "Waiting a bit for containerd to start..."
+echo ''
+echo 'Waiting a bit for containerd to start...'
 sleep 2
 
+echo ''
 SYSTEMD_COLORS=1 journalctl -u ubernerd-containerd -e | tail -n 5
