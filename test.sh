09a1da90b3a20520302191a4666c03f0545fd20a
Jip-Hop <2871973+Jip-Hop@users.noreply.github.com>
2023-09-01 14:44:09 +0200

    Use named volume instead of bind mount
    Allows to use the default snapshotter (OverlayFS) when using Docker Desktop on macOS

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@
 set -euo pipefail
 
 show_error() {
-    docker stop ubernerd-test >/dev/null 2>&1 || true
+    docker stop ubernerd >/dev/null 2>&1 || true
     echo "Error while testing ubernerd installation :("
 }
 
@@ -14,20 +14,18 @@ trap show_error ERR
 
 echo "Start test script!"
 
-# Create a local cache to keep between runs to save time
-mkdir -p /tmp/ubernerd-test
-# Copy the ubernerd.sh script into the test dir (so we can change permissions etc.)
-cp ubernerd.sh /tmp/ubernerd-test/
 # Test ubernerd installation inside a container with systemd init enabled
-docker run -d --name ubernerd-test --privileged -v /tmp/ubernerd-test:/root/ubernerd-test -w /root/ubernerd-test --rm almalinux/9-init
-# Create the symlink, to test running from a different working directory
-docker exec -e CREATE_SYMLINK=1 -e UBERNERD_UPGRADE=1 ubernerd-test ./ubernerd.sh
-
+# Use a named volume to prevent issues with the OverlayFS snapshotter on Docker Desktop on macOS
+# Additionally, this volumes serves as a cache between runs to save time
+# Manually delete this volume to start testing from scratch
+docker run -d --rm --privileged --name ubernerd --mount source=ubernerd,target=/ubernerd -w /ubernerd almalinux/9-init
+# Copy the latest ubernerd.sh script inside the container
+docker cp ubernerd.sh ubernerd:/ubernerd
+# Create the symlink and allow upgrading to the latest nerdctl version
+docker exec -e CREATE_SYMLINK=1 -e UBERNERD_UPGRADE=1 ubernerd ./ubernerd.sh
 # Use -w to run from a different working directory to test the symlink
 # Use --network=host because there is no iptables in this image
-# Use --snapshotter=native because overlayfs doesn't seem to work inside the container:
-# FATA[0000] failed to create shim task: failed to mount rootfs component: invalid argument: unknown
-docker exec -w /tmp ubernerd-test nerdctl run --network=host --snapshotter=native hello-world
-docker stop ubernerd-test >/dev/null 2>&1
+docker exec -w /tmp ubernerd nerdctl run --network=host hello-world
+docker stop ubernerd >/dev/null 2>&1
 
 echo "Successfully passed ubernerd installation test!"
